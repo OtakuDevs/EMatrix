c9d58cf7eea51e7556bce703e015c8441085bffd
Dian Radoslavov <106612784+mokgul@users.noreply.github.com>
2025-05-10 16:48:58 +0300

    initial products view / listing

---
+++ EMatrix.Database/Migrations/20250510110159_menu-option-child-add-icon-property.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMatrix.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class menuoptionchildaddiconproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                table: "MenuOptionChildren",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "MenuOptionChildren");
+        }
+    }
+}
---
+++ EMatrix.ViewModels/Products/ProductListingViewModel.cs
@@ -0,0 +1,15 @@
+namespace EMatrix.ViewModels.Products;
+
+public class ProductListingViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string SubCategory { get; set; } = null!;
+
+    public string NameAlias  { get; set; } = null!;
+
+    public string Icon { get; set; } = null!;
+
+    public float Price { get; set; }
+
+}
